1d46b33e3508b4fed4236f423432c79700b3ae5c
landry-ralph <tchouambiaaudrey@gmail.com>
2025-09-13 10:06:42 +0100

    autre commit

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 
-  // swcMinify: true,
-  // output: "export",
-
   images: {
     remotePatterns: [
       {
